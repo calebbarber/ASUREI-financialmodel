f3cab5bbe23c41bbf54031214ab76a551fe4b8ac
calebbarber <tcalebbarber@gmail.com>
2022-01-19 20:07:16 -0500

    added const path

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.use(express.static('../client/', {index: 'index.html'}))
+app.use(express.static('../client/', {index: 'public/index.html'}));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
